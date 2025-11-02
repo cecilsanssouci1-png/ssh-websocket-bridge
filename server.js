4f1a31b9791c7ce3f6423bd91945c5502e780017
cecilsanssouci1-png <cecilsanssouci1@gmail.com>
2025-11-02 16:33:26 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { WebSocketServer } from "ws";
+import { Client } from "ssh2";
+
+const app = express();
+const server = app.listen(process.env.PORT || 3000, () =>
+  console.log("Server running on port", process.env.PORT)
+);
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  ws.send("?? Connected to SSH bridge");
+
+  const ssh = new Client();
+  ssh
+    .on("ready", () => {
+      ws.send("? SSH connected\n");
+      ssh.shell((err, stream) => {
+        if (err) return ws.send("Shell error: " + err.message);
+
+        ws.on("message", (msg) => stream.write(msg + "\n"));
+        stream.on("data", (data) => ws.send(data.toString()));
+        stream.on("close", () => ws.close());
+      });
+    })
+    .on("error", (err) => ws.send("SSH error: " + err.message))
+    .connect({
+      host: process.env.SSH_HOST,
+      port: 22,
+      username: process.env.SSH_USER,
+      password: process.env.SSH_PASS
+    });
+});
